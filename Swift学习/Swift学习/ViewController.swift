496ef21ddd0de8ed67a672142d2a895cc37eb875
chen <94282425@qq.com>
2015-07-27 23:43:16 +0800

    test1

---
+++ Swift学习/Swift学习/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift学习
+//
+//  Created by 陈尉 on 15/7/27.
+//  Copyright (c) 2015年 diaosi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
